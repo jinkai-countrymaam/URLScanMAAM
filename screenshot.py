16a60d5fad31ec67a3ecc7dd4383090dddfb959e
ykmt8128 <52136734+ykmt8128@users.noreply.github.com>
2021-10-03 21:05:02 +0900

    細かな修正

--- screenshot.py
+++ screenshot.py
@@ -1,5 +1,7 @@
 import urllib.parse
 import urllib.request
+
+
 def download_file(url):
     try:
         with urllib.request.urlopen(url) as web_file:
@@ -8,26 +10,26 @@ def download_file(url):
     except urllib.error.URLError as e:
         print(e)
 
-    """
-    screenshotlayer用のURLを作成する
-    """
+
+# screenshotlayer用のURLを作成する
 def screenshotlayer(access_key, url, args):
     #print("=====SSを撮りたいURLを、加工します======")
     query_string = urllib.parse.urlencode(args)
     url += "&" + query_string
-    return "https://api.screenshotlayer.com/api/capture?access_key=%s&url=%s" % (access_key, url)
+    
+    return f"https://api.screenshotlayer.com/api/capture?access_key={access_key}&url={url}"
+
 
 # SSがほしいURLへアクセスした際のスクショをJPGで返す
 def get_ss_from_url(want_ss_url):
     params = {
         'viewport': '800x600',
         'format': 'jpg',
-    };
-    access_key = "a39aecb96f931ad30c08d912ef054816" #screenshotlayerのaccess_key
+    }
+    access_key = os.environ["SCREENSHOTLAYER_ACCESS_KEY"]  # screenshotlayerのaccess_key
     want_ss_url = urllib.parse.quote(want_ss_url)
-    #print(want_ss_url)
-    access_link = screenshotlayer (access_key, want_ss_url, params)
-
-    #print(access_link)
+    
+    access_link = screenshotlayer(access_key, want_ss_url, params)
     ss_data = download_file(access_link)
+    
     return ss_data
